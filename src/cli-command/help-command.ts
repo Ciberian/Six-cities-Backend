257b278c517f21a897874911988f0b36ca278453
Ciberian <isi04047891@gmail.com>
2022-09-11 23:27:04 +0500

    Добавляет chalk в проект и shebang в cli.ts

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { CliCommandInterface } from './cli-command.interface.js';
 
 export default class HelpCommand implements CliCommandInterface {
@@ -6,13 +7,13 @@ export default class HelpCommand implements CliCommandInterface {
   public async execute(): Promise<void> {
     console.log(`
         Программа для подготовки данных для REST API сервера.
-        Пример:
-            main.js --<command> [--arguments]
-        Команды:
-            --version:                   # Выводит информацию о версии приложения
-            --help:                      # Выводит список и описание всех поддерживаемых аргументов
-            --import <path>:             # Импортирует в базу данных информацию из tsv-файла
-            --generator <n> <path> <url> # Создаёт файл в формате tsv с тестовыми данными.
+        ${chalk.bold.underline('Пример:')}
+            ${chalk.bold.blue.underline('main.js --<command> [--arguments]')}
+        ${chalk.bold.underline('Команды:')}
+            ${chalk.rgb(65, 190, 206)('--version:')}                   # Выводит информацию о версии приложения
+            ${chalk.rgb(100, 24, 244)('--help:')}                      # Выводит список и описание всех поддерживаемых аргументов
+            ${chalk.rgb(112, 23, 155)('--import <path>:')}             # Импортирует в базу данных информацию из tsv-файла
+            ${chalk.rgb(223, 145, 67)('--generator <n> <path> <url>')} # Создаёт файл в формате tsv с тестовыми данными.
         `);
   }
 }
