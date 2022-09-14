d8824397a82ebd3106fcc3d06202323b9fb3d53a
Ciberian <isi04047891@gmail.com>
2022-09-15 04:49:46 +0500

    Рефакторит класс TSVFileReader

--- src/common/file-reader/file-reader.interface.ts
+++ src/common/file-reader/file-reader.interface.ts
@@ -1,7 +1,4 @@
-import { Offer } from '../../types/offer.type';
-
 export interface FileReaderInterface {
 	readonly filename: string;
 	read(): void;
-  toArray(): Offer[];
 }
---
+++ src/utils/common.ts
@@ -0,0 +1,42 @@
+import { Offer } from '../types/offer.type.js';
+
+export const createOffer = (row: string) => {
+  const tokens = row.replace('\n', '').split('\t');
+  const [bedrooms, cityName, cityLatitude, cityLongitude, cityZoom, description, goods, hostAvatar, hostId, hostStatus, hostName, offerId, images, isFavorite, isPremium, offerLatitude, offerLongitude, offerZoom, maxAdults, postDate, previewImage, price, rating, title, type] = tokens;
+
+  return {
+    bedrooms: Number(bedrooms),
+    city: {
+      name: cityName,
+      location: {
+        latitude: Number(cityLatitude),
+        longitude: Number(cityLongitude),
+        zoom: Number(cityZoom)
+      }
+    },
+    description: description,
+    goods: goods.split(';'),
+    host: {
+      avatarUrl: hostAvatar,
+      id: Number(hostId),
+      isPro: Boolean(Number(hostStatus)),
+      name: hostName
+    },
+    id: Number(offerId),
+    images: images.split(';'),
+    isFavorite: Boolean(Number(isFavorite)),
+    isPremium: Boolean(Number(isPremium)),
+    location: {
+      latitude: Number(offerLatitude),
+      longitude: Number(offerLongitude),
+      zoom: Number(offerZoom)
+    },
+    maxAdults: Number(maxAdults),
+    postDate: new Date(postDate),
+    previewImage,
+    price: Number(price),
+    rating: Number(rating),
+    title,
+    type
+  } as Offer;
+};
