0de461436293b4737d582113e83022c32f47c50e
Ciberian <isi04047891@gmail.com>
2022-09-15 05:07:23 +0500

    Рефакторит команду ImportCommand

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -1,22 +1,29 @@
 import TSVFileReader from '../common/file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
+import { createOffer, getErrorMessage } from '../utils/common.js';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
 
+  private onLine(line: string) {
+    const offer = createOffer(line);
+    console.log(offer);
+  }
+
+  private onComplete(count: number) {
+    console.log(`${count} rows imported.`);
+  }
+
   public execute(filename: string): void {
     const fileReader = new TSVFileReader(filename.trim());
+    fileReader.on('line', this.onLine);
+    fileReader.on('end', this.onComplete);
 
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
+      console.log(fileReader.read());
     } catch (err) {
-
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-
-      console.log(`Не удалось импортировать данные из файла по причине: «${err.message}»`);
+      console.log(`Can't read the file: ${getErrorMessage(err)}`);
     }
   }
 }
