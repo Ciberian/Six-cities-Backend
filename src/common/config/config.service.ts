767c2356a00590d42e392329d5c9a94336895a87
Ciberian <isi04047891@gmail.com>
2022-09-18 10:06:23 +0500

    Устанавливает convict и добавляет схему конфигурации

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,9 +1,10 @@
+import { config } from 'dotenv';
 import { ConfigInterface } from './config.interface.js';
-import { config, DotenvParseOutput } from 'dotenv';
 import { LoggerInterface } from '../logger/logger.interface.js';
+import { configSchema, ConfigSchema } from './config.schema.js';
 
 export default class ConfigService implements ConfigInterface {
-  private config: DotenvParseOutput;
+  private config: ConfigSchema;
   private logger: LoggerInterface;
 
   constructor(logger: LoggerInterface) {
@@ -13,12 +14,14 @@ export default class ConfigService implements ConfigInterface {
     if (parsedOutput.error) {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
 
+    configSchema.load({});
+    configSchema.validate({allowed: 'strict', output: this.logger.info});
+    this.config = configSchema.getProperties();
     this.logger.info('.env file found and successfully parsed!');
   }
 
-  public get(key: string): string | undefined {
+  public get<T extends keyof ConfigSchema>(key: T) {
     return this.config[key];
   }
 }
